bff215f6dc8c6bf4f78ccd0bb576b781f56440ae
je-ss-y <nkujess@gmail.com>
2019-07-21 12:52:50 +0200

    added javascript

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function address(){
+    Var DAY =parseFloat(document.getElementById("birth").value);
+    Var MONTH =parseFloat(document.getElementById("birth").value);
+    Var YEAR =parseFloat(document.getElementById("birth").value);
+    Var CENTURY=parseFloat(document.getElementById("birth").value);
+    Var cc=(year-1)/100+1;
+    var birthday=
+    document.getElementById("displayday".innerHTML=Math.floor(birthday));
+
+
+    var male=["Kwasi" "Kwadwo" "Kwabena" "Kwaku" "yaw" "Kofi" "Kwame"];
+    var female=["Akosua" "Adwoa" "Abenaa" "Akua" "yaa" "Afua" "Ama"];
+    var Gender=document.getElementById("displayname").value;
+
+    if(gender==="male"){document.getElementById("displayname").innerHTML=male[Math.floor(birthday)]             
+    };
+      else if(gender==="female"){document.getElementById("displayname").innerHTML=female[Math.floor(birthday)]}
+
+        
+}
